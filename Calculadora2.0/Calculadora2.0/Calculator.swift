1deacc1f0bbf6cd7d2d4f6b1fb90ec2e63a74941
JasmineSantana <34803261+JasmineSantana@users.noreply.github.com>
2024-05-25 16:10:34 -0300

    Add files via upload

---
+++ Calculadora2.0/Calculadora2.0/Calculator.swift
@@ -0,0 +1,38 @@
+//
+//  Calculator.swift
+//  Calculadora2.0
+//
+//  Created by iOSLab on 25/05/24
+//
+
+import Foundation
+
+class Calculadora {
+    
+
+func soma (numberOne:Double, numberTwo:Double) -> Double {
+        
+       return numberOne + numberTwo
+        
+    }
+    func divisão (numberOne:Double, numberTwo:Double) -> Double {
+        
+       return numberOne / numberTwo
+        
+    }
+    
+    func multiplicação (numberOne:Double, numberTwo:Double) -> Double {
+        
+       return numberOne *  numberTwo
+        
+    }
+    func subtração (numberOne:Double, numberTwo:Double) -> Double {
+        
+       return numberOne +  numberTwo
+        
+    }
+    
+}
+
+
+
---
+++ Calculadora2.0/Calculadora2.0/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Calculadora2.0
+//
+//  Created by iOSLab on 25/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let manager: Calc = Calculadora()
+   
+    @IBOutlet weak var numberOneInput: UITextField!
+    @IBOutlet weak var numberTwoInput: UITextField!
+    @IBOutlet weak var resultado: UILabel!
+    
+
+    
+    
+    
+    @IBAction func divisão(_ sender: Any) {
+        
+        
+    }
+    
+    
+    @IBAction func multiplicação(_ sender: Any) {
+    }
+    
+    
+    @IBAction func soma(_ sender: Any) {
+    }
+    
+
+    @IBAction func subtração(_ sender: Any) {
+    }
+    
+}
+
